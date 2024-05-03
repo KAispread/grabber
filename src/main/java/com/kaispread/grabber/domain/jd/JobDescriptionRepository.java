bf8083982791a410574ca51886527c6f369e8d8a
LeeKiWoo <pain0121@naver.com>
2024-05-03 19:54:26 +0900

    feat: add entity repositories

---
+++ src/main/java/com/kaispread/grabber/domain/jd/JobDescriptionRepository.java
@@ -0,0 +1,7 @@
+package com.kaispread.grabber.domain.jd;
+
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface JobDescriptionRepository extends ReactiveCrudRepository<JobDescription, Long> {
+
+}
