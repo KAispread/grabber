3d5066ebbace67257fc7d7719ee5c98f8dd950e6
LeeKiWoo <pain0121@naver.com>
2024-05-06 00:39:04 +0900

    chore: develop profile이 아닌 환경에서 r2dbc-mariadb가 사용되도록 변경

--- src/main/java/com/kaispread/grabber/config/ApplicationConfig.java
+++ src/main/java/com/kaispread/grabber/config/ApplicationConfig.java
@@ -3,10 +3,12 @@ package com.kaispread.grabber.config;
 import com.kaispread.grabber.config.r2dbc.R2dbcConnectionProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @EnableConfigurationProperties({
     R2dbcConnectionProperties.class
 })
+@EnableR2dbcRepositories
 @Configuration
 public class ApplicationConfig {
 
