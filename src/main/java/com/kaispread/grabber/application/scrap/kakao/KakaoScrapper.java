9d550efb920bf5a0409afc95af65024107c89c19
LeeKiWoo <pain0121@naver.com>
2024-05-06 22:33:50 +0900

    refactor: Scrapper 클래스가 WebClient 대신 Custom ApiCaller를 의존하도록 변경

--- src/main/java/com/kaispread/grabber/application/scrap/kakao/KakaoScrapper.java
+++ src/main/java/com/kaispread/grabber/application/scrap/kakao/KakaoScrapper.java
@@ -1,19 +1,19 @@
 package com.kaispread.grabber.application.scrap.kakao;
 
 
+import com.kaispread.grabber.application.api.ApiCaller;
 import com.kaispread.grabber.application.dto.company.CompanyDto;
 import com.kaispread.grabber.application.dto.scrap.ScrapJdDto;
 import com.kaispread.grabber.application.scrap.JobDescriptionScrapper;
 import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 public class KakaoScrapper implements JobDescriptionScrapper {
 
-    private final WebClient webClient;
+    private final ApiCaller apiCaller;
 
     @Override
     public Mono<List<ScrapJdDto>> scrap(final CompanyDto companyDto, final Map<String, String> header) {
--- src/main/java/com/kaispread/grabber/config/bean/ScrapperBeanConfig.java
+++ src/main/java/com/kaispread/grabber/config/bean/ScrapperBeanConfig.java
@@ -2,6 +2,7 @@ package com.kaispread.grabber.config.bean;
 
 import static com.kaispread.grabber.application.scrap.ScrapperType.KAKAO_CORE;
 
+import com.kaispread.grabber.application.api.ApiCaller;
 import com.kaispread.grabber.application.scrap.Scrapper;
 import com.kaispread.grabber.application.scrap.ScrapperFactory;
 import com.kaispread.grabber.application.scrap.ScrapperType;
@@ -9,22 +10,21 @@ import com.kaispread.grabber.application.scrap.kakao.KakaoScrapper;
 import java.util.Map;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ScrapperBeanConfig {
 
     @Bean
-    public ScrapperFactory scrapperFactory(WebClient webClient) {
+    public ScrapperFactory scrapperFactory(ApiCaller apiCaller) {
         Map<ScrapperType, Scrapper> scrapperMap = Map.ofEntries(
-            Map.entry(KAKAO_CORE, kakaoScrapper(webClient))
+            Map.entry(KAKAO_CORE, kakaoScrapper(apiCaller))
         );
 
         return new ScrapperFactory(scrapperMap);
     }
 
     @Bean
-    public Scrapper kakaoScrapper(WebClient webClient) {
-        return new KakaoScrapper(webClient);
+    public Scrapper kakaoScrapper(ApiCaller apiCaller) {
+        return new KakaoScrapper(apiCaller);
     }
 }
