b321e13830472c562b61ea3ebe03683005e6e0b0
LeeKiWoo <pain0121@naver.com>
2024-05-07 18:25:51 +0900

    test: test 환경에서 H2-R2DBC 를 사용하도록 수정

--- src/test/java/com/kaispread/grabber/base/support/IntegrationTestSupport.java
+++ src/test/java/com/kaispread/grabber/base/support/IntegrationTestSupport.java
@@ -1,7 +1,9 @@
 package com.kaispread.grabber.base.support;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("develop")
 @SpringBootTest
 public abstract class IntegrationTestSupport {
 }
