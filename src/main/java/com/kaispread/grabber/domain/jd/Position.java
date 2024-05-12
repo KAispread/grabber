04f91fdaf4eb696d9397968388bebc3ba8b0d9df
LeeKiWoo <pain0121@naver.com>
2024-05-12 11:26:32 +0900

    feat: Position 인스턴스를 얻기 위한 정적 팩토리 메서드 추가

--- src/main/java/com/kaispread/grabber/domain/jd/Position.java
+++ src/main/java/com/kaispread/grabber/domain/jd/Position.java
@@ -1,19 +1,29 @@
 package com.kaispread.grabber.domain.jd;
 
+import java.util.Arrays;
 import java.util.List;
 import lombok.Getter;
 
 @Getter
 public enum Position {
-    BACKEND (List.of("backend")),
-    FRONTEND (List.of("frontend")),
-    DEVOPS (List.of("devops")),
-    DATA (List.of("data_engineer")),
+    FRONTEND (List.of("frontend", "front-end", "front_end", "프론트엔드", "프론트앤드", "프런트엔드", "프론트")),
+    BACKEND (List.of("backend", "back-end", "back_end", "백엔드", "백앤드", "서버", "server")),
+    DEVOPS (List.of("devops", "devsecops", "dev-ops", "데브옵스", "infra", "infrastructure", "인프라", "AWS", "GCP", "Azure", "클라우드", "cloud")),
+    APP(List.of("안드로이드", "android", "ios", "app", "앱", "react native", "flutter")),
     ETC (null);
 
     private final List<String> names;
 
     Position(List<String> names) {
         this.names = names;
+    }
+
+    public static Position getInstance(final String jobTitle) {
+        String lowerCaseJobTitle = jobTitle.toLowerCase();
+        return Arrays.stream(Position.values())
+            .filter(position -> position.getNames() != null)
+            .filter(position -> position.getNames().stream().anyMatch(lowerCaseJobTitle::contains))
+            .findFirst()
+            .orElse(ETC);
     }
 }
