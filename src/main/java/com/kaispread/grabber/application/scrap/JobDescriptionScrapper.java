f248593f2d266e11ca737c4436551b3b1bb55c9b
LeeKiWoo <pain0121@naver.com>
2024-05-12 11:23:37 +0900

    refactor: Jd Scrapper 반환 타입 수정 * Mono<List<ScrapJdDto>> -> Flux<ScrapJdDto>

--- src/main/java/com/kaispread/grabber/application/scrap/JobDescriptionScrapper.java
+++ src/main/java/com/kaispread/grabber/application/scrap/JobDescriptionScrapper.java
@@ -2,10 +2,9 @@ package com.kaispread.grabber.application.scrap;
 
 import com.kaispread.grabber.application.dto.company.CompanyDto;
 import com.kaispread.grabber.application.dto.scrap.ScrapJdDto;
-import java.util.List;
 import java.util.Map;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
 
 public interface JobDescriptionScrapper extends Scrapper {
-    Mono<List<ScrapJdDto>> scrap(CompanyDto companyDto, Map<String, String> header);
+    Flux<ScrapJdDto> scrap(CompanyDto companyDto, Map<String, String> header);
 }
