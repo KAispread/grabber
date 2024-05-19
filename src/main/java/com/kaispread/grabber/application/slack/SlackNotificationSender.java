f0bb9ac3f8753755c7edb32268895a7b4f20ba54
LeeKiWoo <pain0121@naver.com>
2024-05-19 14:29:43 +0900

    feat: slack 메시지 전송 Http Interface 추가 (webClient base)

---
+++ src/main/java/com/kaispread/grabber/application/slack/SlackNotificationSender.java
@@ -0,0 +1,16 @@
+package com.kaispread.grabber.application.slack;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PostExchange;
+import reactor.core.publisher.Mono;
+
+@HttpExchange(contentType = "application/json")
+public interface SlackNotificationSender {
+
+    @PostExchange(url = "/T073P744H8F/B0740SGGB61/42td9bNDWtLfuGDyPkeTOnrv")
+    Mono<String> postToDailyChannel(@RequestBody SlackMessage message);
+
+    @PostExchange(url = "/T073P744H8F/B0743QKC9DG/4kGa3j5TQqtT8PYqEEedcAvk")
+    Mono<String> postToTestChannel(@RequestBody SlackMessage message);
+}
