4b26e3619958e5eef38c608059710864331b197a
LeeKiWoo <pain0121@naver.com>
2024-05-22 23:43:55 +0900

    feat: 날짜 기준 ExceptionEvent 엔티티 조회 쿼리 추가

--- src/main/java/com/kaispread/grabber/domain/event/ExceptionEventRepository.java
+++ src/main/java/com/kaispread/grabber/domain/event/ExceptionEventRepository.java
@@ -1,6 +1,9 @@
 package com.kaispread.grabber.domain.event;
 
+import java.time.LocalDate;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 public interface ExceptionEventRepository extends ReactiveCrudRepository<ExceptionEvent, Long>  {
+    Flux<ExceptionEvent> findByCreatedDate(LocalDate createdDate);
 }
